9cb148d7faebd35cddd2ff5ab49d1454c1033724
Staline Ngoma <ngomastaline@gmail.com>
2024-07-04 08:11:45 -0500

    07-04-2024-08:14: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [score, setScore] = useState('10')
+  const [comment, setComment] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (Number(score) <= 5 && comment.length <= 10) {
+      alert(`A score of ${score} usually indicates that we did not perform well.\nPlease indicate in the comment why your experience was poor 🙂`)
+      return
+    }
+    console.log('Form submitted!')
+    setComment('')
+    setScore('10')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Made With React JS</h1>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <div className='DivField'>
+              <h1>Provide Feedback</h1>
+              <div>
+                <label>Comment:</label>
+                <br />
+                <textarea
+                  className='TextareaLabel'
+                  rows='12'
+                  cols='50'
+                  value={comment}
+                  onChange={e => setComment(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>Score: {score} ⭐</label>
+                <br />
+                <input
+                  className='InpuLabel'
+                  type='range'
+                  min='0'
+                  max='10'
+                  value={score}
+                  onChange={(e) => setScore(e.target.value)}
+                />
+              </div>
+              <button disabled={!comment} className='Submit-btn' type='submit'>Submit</button>
+            </div>
+          </fieldset>
+        </form>
       </header>
     </div>
   );
